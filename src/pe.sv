5697c4e67f8444545a5579a536cd791fa35e42a2
mwhalm <mwhalm@coe-ece-rubichest.ou.ad3.ucdavis.edu>
2026-05-01 01:47:41 -0700

    Work on pe.sv

--- src/pe.sv
+++ src/pe.sv
@@ -2,11 +2,11 @@ module pe #(
 	parameter IA_WIDTH = 8,
 	parameter W_WIDTH = 8,
 	parameter OA_WIDTH = 20
-	)(
+)(
 	input logic clk,
 	input logic rst,
-	input logic pe_en,
-	input state,
+	input logic en,
+	input logic valid,
 	input logic signed [IA_WIDTH - 1 : 0] ia_in,
 	input logic signed [W_WIDTH - 1 : 0] w_in,
 	input logic signed [OA_WIDTH - 1 : 0] oa_in,
@@ -20,15 +20,22 @@ logic [W_WIDTH - 1 : 0] weight;
 logic [IA_WIDTH - 1 : 0] input_data;
 
 always_ff @(posedge clk) begin
-	if (!rst) begin
+	if (rst) begin
 		ia_out <= '0;
 		w_out <= '0;
 		oa_out <= '0;
 	end else begin
 		ia_out <= ia_in;
 		w_out <= w_in;
-		if (pe_en) begin
-			oa_out <= oa_in + (ia_in * w_in);
+
+		if (load_weight) begin
+			weight <= w_in;
+		end else begin
+			weight <= weight;
+		end
+		
+		if (en && valid) begin
+			oa_out <= oa_in + (ia_in * weight);
 		end else begin
 			oa_out <= oa_in;
 		end
