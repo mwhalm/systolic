778837c61c18a69de91d163b036cea33eb9a89da
Brinda Puri <45851150+BrindaPuri@users.noreply.github.com>
2026-06-07 14:58:32 -0700

    quantizer with safety gaurds

--- rtl/dynamic_quantizer.sv
+++ rtl/dynamic_quantizer.sv
@@ -1,4 +1,3 @@
-//Per-tile dynamic fixed-point quantization
 module dynamic_quantizer #(
     parameter N = 8,
     parameter IN_W = 16,
@@ -14,27 +13,31 @@ module dynamic_quantizer #(
     logic signed [IN_W-1:0] max_val;
 
     always_comb begin
-        max_val = 0;
-        for (i = 0; i < N; i++) begin
-            for (j = 0; j < N; j++) begin
-                abs_val = in_tile[i][j][IN_W-1] ?-in_tile[i][j]:in_tile[i][j];
+    	max_val = 0;
+    	for (i = 0; i < N; i++) begin
+		for (j = 0; j < N; j++) begin
+			abs_val = in_tile[i][j][IN_W-1] ?-in_tile[i][j]:in_tile[i][j];
+			if (abs_val > max_val) 
+				max_val = abs_val;
+		end
+   	end
 
-                if (abs_val > max_val) 
-                    max_val = abs_val;
-            end
-        end
-
-        shift = 0;
-        for (int k = IN_W; k >= OUT_W; k--) begin
-            if (max_val > (1 << k))
-                shift = k - OUT_W;
-        end
-
-        //Quantize
-        for (i = 0; i < N; i++) begin
-            for (j = 0; j < N; j++) begin
-                out_tile[i][j] = in_tile[i][j] >>> shift;
-            end
-        end
+   	shift = 0;
+   	for (int s = 0; s < (IN_W - OUT_W + 1); s++) begin
+		if ((max_val >> s) > ((1 << (OUT_W-1)) - 1))
+			shift = s + 1;
+		if (shift > IN_W-1)
+			shift = IN_W-1;
+   	end
+   
+   	for (i = 0; i < N; i++) begin
+		for (j = 0; j < N; j++) begin
+			if(shift == 0) begin
+				out_tile[i][j] = in_tile[i][j];
+			end else begin
+				out_tile[i][j] = (in_tile[i][j] + (1 << (shift-1))) >>> shift;
+			end
+		end
+	end
     end
 endmodule
