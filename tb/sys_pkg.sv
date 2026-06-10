f7631e70db5687bc86cffb8dc0633bd1e797a659
mwhalm <118238023+mwhalm@users.noreply.github.com>
2026-06-10 01:25:25 -0700

    Comments in sys_pkg.sv

--- tb/sys_pkg.sv
+++ tb/sys_pkg.sv
@@ -1,17 +1,17 @@
 package sys_pkg;
     import uvm_pkg::*;
     `include "uvm_macros.svh"
-    parameter int N = 8;
-    parameter int M_SIZE = 40;
-    parameter int K_SIZE = 40;
-    parameter int N_SIZE = 40;
-    parameter int FILTER_SIZE = 2;
-    parameter int H = 40;
-    parameter int W = 3;
+    parameter int N = 8;    // systolic array size (N x N)
+    parameter int M_SIZE = 40;    // A ~ M x K
+    parameter int K_SIZE = 40;    // C ~ M x N
+    parameter int N_SIZE = 40;    // B ~ K x N
+    parameter int FILTER_SIZE = 2;    // square convolution filter size
+    parameter int H = 40;    // height of convolution input matrix
+    parameter int W = 3;    // width of convolution input matrix
     parameter int R = FILTER_SIZE;
     parameter int S = FILTER_SIZE;
-    parameter int P = H - R + 1;
-    parameter int Q = W - S + 1;
+    parameter int P = H - R + 1;    // output convolution height
+    parameter int Q = W - S + 1;    // output convolution width
     parameter int IA_WIDTH = 8;
     parameter int W_WIDTH = 8;
 
